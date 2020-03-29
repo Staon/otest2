fadfcefa7accc1879a3ce3d4696ef0adc75adde8
Staon <stareko@email.cz>
2020-03-29 14:48:55 +0200

    Changed the CI image for the otest2's include path configuration

--- cidocker/build-ci.sh
+++ cidocker/build-ci.sh
@@ -5,5 +5,6 @@ docker_args=--no-cache
 develop_image=staon/otest2-ci:latest
 
 # -- The development image
+cp ../otest2/otest2_discovery.sh .
 docker build "$docker_args" -t "$develop_image" -f dockerfile.ci . || exit 1
 

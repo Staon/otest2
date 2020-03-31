47b8781dc58df61d183c081d6fec3b2296a33dc2
Staon <stareko@email.cz>
2020-03-31 17:34:18 +0200

    A cmake function generating the default test main function

--- cmake/target-sources.cmake
+++ cmake/target-sources.cmake
@@ -31,3 +31,27 @@ function(TARGET_OTEST2_SOURCES target)
     set_directory_properties(PROPERTIES ADDITIONAL_MAKE_CLEAN_FILES ${gensrc})
   endforeach()
 endfunction()
+
+# -- name of the default template of the main.cpp
+set(OTEST2_MAIN_TEMPLATE "${CMAKE_CURRENT_LIST_DIR}/template-main.cpp")
+
+# generate default test main file
+#
+# Usage target_otest2_main(<target> [FILE genfile.cpp])
+function(TARGET_OTEST2_MAIN target)
+  set(one_value_args FILE)
+  cmake_parse_arguments(OTEST2 "" "${one_value_args}" "" ${ARGN})
+  
+  if(OTEST2_FILE)
+    set(main_file "${CMAKE_CURRENT_SOURCE_DIR}/${OTEST2_FILE)")
+  else()
+    set(main_file "${CMAKE_CURRENT_SOURCE_DIR}/main.cpp")
+  endif()
+
+  add_custom_command(
+      OUTPUT ${main_file}
+      COMMAND cat ${OTEST2_MAIN_TEMPLATE} > ${main_file}
+  )
+  target_sources(${target} PRIVATE ${main_file})
+  set_directory_properties(PROPERTIES ADDITIONAL_MAKE_CLEAN_FILES ${main_file})
+endfunction()
